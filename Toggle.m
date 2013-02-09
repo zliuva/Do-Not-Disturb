ff3d68c566a9fae0f4c1dc8c7ea757938985b4a9
softboysxp <softboysxp@gmail.com>
2013-02-08 19:50:49 -0500

    initial import

---
+++ Toggle.m
@@ -0,0 +1,32 @@
+@interface BBSettingsGateway : NSObject
+- (void)setBehaviorOverrideStatus:(BOOL)enabled;
+- (void)setActiveBehaviorOverrideTypesChangeHandler:(void (^)(BOOL))block;
+@end
+
+static BBSettingsGateway *bbGateway;
+static BOOL isDND = NO;
+
+BOOL isCapable() {
+	return YES;
+}
+
+BOOL isEnabled() {
+	return isDND;
+}
+
+void setState(BOOL enabled) {
+	[bbGateway setBehaviorOverrideStatus:enabled];
+}
+
+float getDelayTime() {
+	return .0f;
+}
+
+__attribute__((constructor)) static void init() {
+	bbGateway = [[BBSettingsGateway alloc] init];
+
+	[bbGateway setActiveBehaviorOverrideTypesChangeHandler:^(BOOL enabled) {
+		isDND = enabled;
+	}];
+}
+
