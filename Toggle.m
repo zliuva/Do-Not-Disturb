4eceb6702676fef4572b4e52e14d46b93e7513be
softboysxp <softboysxp@gmail.com>
2013-02-11 17:07:09 -0500

    NCSettings support

--- Toggle.m
+++ Toggle.m
@@ -14,17 +14,23 @@ BOOL isEnabled() {
 	return isDND;
 }
 
-BOOL getStateFast() {
-	return isDND;
-}
-
 void setState(BOOL enabled) {
 	[bbGateway setBehaviorOverrideStatus:enabled];
+}
+
+#ifdef NC_SETTINGS
+NSString *localizedName() {
+	return @"Do Not Disturb";
+}
+#else
+BOOL getStateFast() {
+	return isDND;
 }
 
 float getDelayTime() {
 	return .0f;
 }
+#endif
 
 __attribute__((constructor)) static void init() {
 	bbGateway = [[BBSettingsGateway alloc] init];
