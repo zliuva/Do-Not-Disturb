12287f6cebf20470ce4f888ea70fc94de373a6fe
softboysxp <softboysxp@gmail.com>
2013-02-12 04:42:33 -0500

    ugly hack for NotificationCenter

--- Toggle.m
+++ Toggle.m
@@ -1,9 +1,17 @@
+#include <objc/runtime.h>
+
 @interface BBSettingsGateway : NSObject
 - (void)setBehaviorOverrideStatus:(BOOL)enabled;
 - (void)setActiveBehaviorOverrideTypesChangeHandler:(void (^)(BOOL))block;
 @end
 
+@interface SBStatusBarDataManager : NSObject
++ (id)sharedDataManager;
+- (void)setStatusBarItem:(int)arg1 enabled:(BOOL)arg2;
+@end
+
 static BBSettingsGateway *bbGateway;
+static SBStatusBarDataManager *manager;
 static BOOL isDND = NO;
 
 BOOL isCapable() {
@@ -16,6 +24,19 @@ BOOL isEnabled() {
 
 void setState(BOOL enabled) {
 	[bbGateway setBehaviorOverrideStatus:enabled];
+
+	/**
+	 * ugly fix for when toggle is inside NotificationCenter
+	 * somehow the icon disappears when NC is closed
+	 * so wait for a while and reset the icon (must disable then enable)
+	 * magical number 1 corresponds to "quiet" icon
+	 */
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.15 * NSEC_PER_SEC), dispatch_get_current_queue(), ^() {
+		if (enabled) {
+			[manager setStatusBarItem:1 enabled:NO];
+		}
+		[manager setStatusBarItem:1 enabled:enabled];
+	});
 }
 
 #ifdef NC_SETTINGS
@@ -38,5 +59,7 @@ __attribute__((constructor)) static void init() {
 	[bbGateway setActiveBehaviorOverrideTypesChangeHandler:^(BOOL enabled) {
 		isDND = enabled;
 	}];
+
+	manager = [objc_getClass("SBStatusBarDataManager") sharedDataManager];
 }
 
